f607994bfa6feb4b39fd889ef75781e8bfac641b
CristianoJSilva <cristiano.jsilva@hotmail.com>
2021-07-27 20:09:52 -0300

    Alteração de banco 27/07/2021-20:09

--- src/main/java/cristianosilva/APIHotmart/model/Compra.java
+++ src/main/java/cristianosilva/APIHotmart/model/Compra.java
@@ -18,7 +18,7 @@ import javax.persistence.Table;
 public class Compra {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.AUTO)
     @Column(name = "id")
     private Long id;
 
